a75ad9fe9491f7dea048d2abfba649d30b3cffb9
Calos01 <ca_los01@hotmail.com>
2023-08-06 21:38:31 -0500

    estructura components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CardMusicComponent } from './components/card-music/card-music.component';
+import { NavTopComponent } from './components/nav-top/nav-top.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    PlaylistComponent,
+    SignupComponent,
+    LoginComponent,
+    CardMusicComponent,
+    NavTopComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
