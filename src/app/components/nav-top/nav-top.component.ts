afa160a6b9de58658689e1a1a7e0c786a70ccd96
Calos01 <ca_los01@hotmail.com>
2023-08-11 09:48:32 -0500

    search and login avance

--- src/app/components/nav-top/nav-top.component.ts
+++ src/app/components/nav-top/nav-top.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-top',
   templateUrl: './nav-top.component.html',
   styleUrls: ['./nav-top.component.css']
 })
-export class NavTopComponent {
+export class NavTopComponent implements OnInit{
+  isLoginRoute: boolean = false;
+
+  constructor(private router:Router) {
+  }
+  ngOnInit(): void {
+    
+  }
+  redirectLogin(){
+    this.router.navigateByUrl('/login')
+  }
 
 }
